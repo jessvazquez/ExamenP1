55a3c6c15285080740df0d5bce09ba61e85c0d56
jessvazquez <31674161+jessvazquez@users.noreply.github.com>
2019-09-18 19:37:00 -0500

    aplicacion "terminada"
    Icono insertado

--- app/src/main/java/com/example/examen/MundoWeb.java
+++ app/src/main/java/com/example/examen/MundoWeb.java
@@ -2,38 +2,49 @@ package com.example.examen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MundoWeb extends AppCompatActivity {
     private WebView mWebView;
     Button btnAbrirWeb;
+
+    String sdireccion;
+    EditText edireccion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mundo_web);
 
 
-
         btnAbrirWeb = (Button) findViewById(R.id.btn_AbrirWeb);
+
 
         btnAbrirWeb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(sdireccion);
+
                 // INI AGREGADO
+                edireccion = findViewById(R.id.id_direccion);
+                sdireccion = edireccion.getText().toString();
                 mWebView = (WebView) findViewById(R.id.activity_main_webview);
                 // Activamos jаvascript
                 WebSettings webSettings = mWebView.getSettings();
                 // Url que carga la app (webview)
-                mWebView.loadUrl("https://google.com/");
+                mWebView.loadUrl(sdireccion);
                 // Forzamos el webview para que abra los enlaces internos dentro de la la APP
                 mWebView.setWebViewClient(new WebViewClient());
                 // Forzamos el webview para que abra los enlaces externos en el navegador
                 mWebView.setWebViewClient(new MyAppWebViewClient());
+               // intent.putExtra("nombre", sdireccion);
             }
         });
     }
